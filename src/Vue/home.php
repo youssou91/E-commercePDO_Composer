1ecfcfccdd516cb1aa1bc03c8375a12b63ab7aed
Youssou Gning <youssougning@gmail.com>
2025-12-01 19:27:01 -0500

    fermeture modal appercu produit

--- src/Vue/home.php
+++ src/Vue/home.php
@@ -609,5 +609,9 @@
         function openModal(id) {
             document.getElementById(id).classList.remove('hidden');
         }
+
+        function closeModal(id) {
+            document.getElementById(id).classList.add('hidden');
+        }
     </script>
 </html>
